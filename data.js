8e61b1c3d7ba7c916b519eb133da671b4f9f57dc
anniehu17 <anniegracehu@gmail.com>
2021-09-05 11:11:21 -0400

    adding likes

--- data.js
+++ data.js
@@ -11,14 +11,14 @@ var defaultThreads = [
                 author: "Man Oh Man",
                 date: Date.now(),
                 content: "Aw man, that is unfortunately not recyclable because it's a halogen light bulb. They contain halogen light bulbs and are tough to recycle. In 43221, you can just put it in your trash cart for disposal.",
-                likes: "0"
+                likes: "924"
             },
             {
                 id:'12',
                 author: "No Waste Gang",
                 date: Date.now(),
                 content: "While Man Oh Man is right, you can also repurpose that light bulb into an air plant bulb. Just put an air plant and some water inside, and dangle it from your ceiling with a string. It looks really cool and you get a new home for a plant :).",
-                likes: "0"
+                likes: "877"
             }
         ],
         img: "lightbulb.jpg"
@@ -35,14 +35,14 @@ var defaultThreads = [
                 author: "Rick Astley",
                 date: Date.now(),
                 content: "Never gonna give recycling up! Rock on, bud, that is totally recyclable!",
-                likes: "0"
+                likes: "690"
             },
             {
                 id:'22',
                 author: "Agora5",
                 date: Date.now(),
                 content: "Rick's totally right! And it's reusable too - you can fill that bad boi up and keep on using it!",
-                likes: "0"
+                likes: "420"
             }
         ],
         img: "plastic-water-bottle.jpg"
@@ -55,4 +55,8 @@ if (localStorage && localStorage.getItem('threads')) {
 } else {
     threads = defaultThreads;
     localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
+
+if (!localStorage.getItem('pts')) {
+    localStorage.setItem('pts', '0');
 }
